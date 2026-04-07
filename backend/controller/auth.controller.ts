0f1c3bc30f2d2f969c8246174daad2b7bf9855e5
Prateek Yadu <prateek@prateekyadu.com>
2026-04-07 12:53:32 +0530

    feat(backend): added logs to auth route

--- backend/controller/auth.controller.ts
+++ backend/controller/auth.controller.ts
@@ -5,11 +5,16 @@ import send from "../utils/response/index.js";
 import { pool } from '../db/config.js';
 import jwt from 'jsonwebtoken';
 import { isVailedEmail, isVailedPassword, isVailedUsername } from '../utils/validators/index.js';
+import { logger } from '../utils/logger/logger.utils.js';
 
 interface userData {
     name: string;
     email: string;
     profileImage: string;
+}
+
+interface customRequest extends Request {
+    id?: string;
 }
 
 export const Login = async (req: Request, res: Response) => {
@@ -20,13 +25,20 @@ export const Login = async (req: Request, res: Response) => {
         const isEmailVailed = isVailedEmail(email);
 
         if (!isEmailVailed) {
+
+            logger.log("auth", { ip: req.ip, message: `[validator]: Failed email check`, type: "error", route: "POST /login" });
             return send.badRequest(res, "Invailed credentials");
         }
 
         // checks if user exists
         const [exists, fields]: any = await pool.query('SELECT id, name, profile_image, password FROM users WHERE email = ?', [email]);
 
+
         if (exists.length != 0) {
+
+            // stores user id
+            const userId = exists[0].id;
+
             // checks password is vailed or not
             const vailed = await bcrypt.compare(password, exists[0].password);
 
@@ -52,15 +64,23 @@ export const Login = async (req: Request, res: Response) => {
                     secure: process.env.IN_PROD == "true" && true || false
                 });
 
+                logger.log("auth", { ip: req.ip, message: "Successfull user login", type: "success", userId: userId, route: "POST /login" });
+
                 // sends response
                 send.ok(res, "User Authenticated Successfully", { ...userInfo });
             } else {
+
+                logger.log("auth", { ip: req.ip, message: `Failed login attempt`, type: "error", userId: userId, route: "POST /login" });
                 send.unauthorized(res, "Invailed Credentials");
             }
         } else {
+
+            logger.log("auth", { ip: req.ip, message: `Login attempt from invailed email address ${email}`, type: "error", route: "POST /login" });
             send.unauthorized(res, "Invailed Credentials");
         }
     } catch (error) {
+
+        logger.log("auth", { ip: req.ip, message: `Internal Error`, type: "error", route: "POST /login" });
         send.internalError(res);
     }
 };
@@ -76,6 +96,7 @@ export const Logout = async (req: Request, res: Response) => {
             send.ok(res, "User already logged out");
         }
     } catch (error) {
+        logger.log("auth", { ip: req.ip, message: `Internal Error`, type: "error", route: "POST /logout" });
         send.internalError(res);
     }
 };
@@ -90,14 +111,17 @@ export const Register = async (req: Request, res: Response) => {
         const isUsernameVailed = isVailedUsername(name);
 
         if (!isUsernameVailed) {
+            logger.log("auth", { ip: req.ip, message: `[validator]: Failed username check`, type: "error", route: "POST /register" });
             return send.badRequest(res, "Username should not be less than 4 characters");
         }
 
         if (!isEmailVailed) {
+            logger.log("auth", { ip: req.ip, message: `[validator]: Failed email check`, type: "error", route: "POST /register" });
             return send.badRequest(res, "Please enter vailed email");
         }
 
         if (!isPasswordVailed) {
+            logger.log("auth", { ip: req.ip, message: `[validator]: Failed password check`, type: "error", route: "POST /register" });
             return send.badRequest(res, "Password length must be atleast 8 characters");
         }
 
@@ -106,6 +130,9 @@ export const Register = async (req: Request, res: Response) => {
         const [exists, fields]: any = await pool.query('SELECT email FROM users WHERE email = ?', [email]);
 
         if (exists.length != 0) {
+
+            logger.log("auth", { ip: req.ip, message: `User already exists with email ${email}`, type: "error", route: "POST /register" });
+
             // return duplicate user error
             send.conflict(res, "User Already Exists");
         } else {
@@ -117,13 +144,16 @@ export const Register = async (req: Request, res: Response) => {
             const values = [userId, name, email, hash];
             const [result, fields] = await pool.execute(sql, values);
 
+            logger.log("auth", { ip: req.ip, message: `User Created successfully with email address ${email}`, type: "success", route: "POST /register" });
             send.created(res, "User Created Successfuly");
         }
     } catch (error) {
+        logger.log("auth", { ip: req.ip, message: `Internal server error`, type: "error", route: "POST /register" });
         send.internalError(res);
     }
 };
 
-export const Validate = async (req: Request, res: Response) => {
+export const Validate = async (req: customRequest, res: Response) => {
+    logger.log("auth", { ip: req.ip, message: `Hit success`, type: "success", route: "POST /validate", userId: req.id });
     send.ok(res); // sends ok if middleware validates the token 
 };
