2b81b453dd4aaa0e20f6ffb589fcbc41e9ca0c19
Prateek Yadu <prateek@prateekyadu.com>
2026-05-17 22:29:10 +0530

    feat(backendr): implemented logging functionality for workers

--- backend/lib/logger.utils.ts
+++ backend/lib/logger.utils.ts
@@ -12,6 +12,13 @@ interface log {
     route: string;
 }
 
+interface workerLog {
+    timestamp?: number;
+    type: "success" | "error" | "info" ,
+    message: string,
+    error?: string
+}
+
 export const logger = {
     async log(filename: "auth" | "billing" | "instance" | "profile", log: log) {
 
@@ -34,5 +41,30 @@ export const logger = {
         }
 
 
+    }, worker: {
+        async log(worker: "event" | "expiry" | "lifecycle" | "provision" | "sync", log: workerLog){
+            try {
+                const __filename = fileURLToPath(import.meta.url)
+                const __dirname = dirname(__filename)
+
+                const outDir = path.join(__dirname, "..", "..", "logs", "workers", worker)
+
+                log.timestamp = Date.now();
+
+                if(!fs.existsSync(outDir)) {
+                    fs.mkdirSync(outDir, {
+                        recursive: true
+                    })
+                }
+
+                fs.appendFile(`${outDir}/${worker}.worker.log`, JSON.stringify(log) + "\n", (err:any) =>{
+                    if(err){
+                        throw new Error(err)
+                    }
+                })
+            }catch (error: any){
+                throw new Error("ERROR: Failed to wite log reason: ", error)
+            }
+        }
     }
 };
