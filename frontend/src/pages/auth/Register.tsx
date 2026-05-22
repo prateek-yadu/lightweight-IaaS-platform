8675f49a1ccf67a5902433e8651a451bdea837f7
Prateek Yadu <prateek@prateekyadu.com>
2026-05-22 22:40:21 +0530

    chore(frontend): redesigned register page

--- frontend/src/pages/auth/Register.tsx
+++ frontend/src/pages/auth/Register.tsx
@@ -1,27 +1,31 @@
+import { RegisterForm } from "@/components/register-form";
 import Branding from "../../branding.json";
-import { RegisterForm } from "../../components/auth/RegisterForm";
+import { Link } from "react-router";
 
 export default function Register() {
   const appName = Branding.AppName; // gets app name from @/src/branding.json
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 min-h-screen bg-accent/[1%] ">
-      <div className="flex flex-col">
-        {/* Branding */}
-        <h2 className="logo font-semibold text-xl text-accent px-8 py-8 lg:px-12">
-          {appName}
-        </h2>
-
-        <div className="w-[70%] lg:w-[60%] xl:w-[45%] m-auto">
-          {/* Register Form  */}
-          <RegisterForm />
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <Link to="/" className="flex items-center gap-2 font-medium">
+            <div className="flex size-6 items-center justify-center rounded-md text-primary-foreground">
+              <img src="/brandIcon.svg" />
+            </div>
+            {appName}
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-sm">
+            <RegisterForm />
+          </div>
         </div>
       </div>
-
-      <div className="hidden lg:flex">
+      <div className="relative hidden bg-muted lg:block">
         <img
           src="/images/auth.jpg"
-          alt=""
-          className="object-center object-cover"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover"
         />
       </div>
     </div>
