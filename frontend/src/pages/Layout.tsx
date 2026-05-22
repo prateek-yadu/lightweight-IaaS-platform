12e11b871e362d44af114c3fb0f639761f74726c
Prateek Yadu <prateek@prateekyadu.com>
2026-05-22 22:43:45 +0530

    chore(frontend): added dark mode and light mode theme support

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,11 +1,14 @@
+import { ThemeProvider } from '@/components/theme-provider';
+import { Toaster } from '@/components/ui/sonner';
 import { Outlet } from 'react-router';
-import { Toaster } from 'sonner';
 
 export default function Layout() {
     return (
         <>
+        <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
             <Toaster position='top-center' duration={1500}/> {/* sonner toast */}
             <Outlet />
+        </ThemeProvider>
         </>
     );
 }
