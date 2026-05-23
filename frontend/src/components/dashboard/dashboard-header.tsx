a6b66e8d8dfa1febba80acc469608520a612153a
Prateek Yadu <prateek@prateekyadu.com>
2026-05-23 23:46:29 +0530

    chore(frontend): added dashboard header component

---
+++ frontend/src/components/dashboard/dashboard-header.tsx
@@ -0,0 +1,16 @@
+import { type ReactNode } from "react";
+import { TypographyH3 } from "../Typography/Typography";
+
+type DashboardHeaderProps = {
+  title: string;
+  action?: ReactNode;
+};
+
+export default function DashboardHeader({ title, action }: DashboardHeaderProps) {
+  return (
+    <div className="flex items-center justify-between mt-4 mb-6">
+      <TypographyH3>{title}</TypographyH3>
+      {action && action}
+    </div>
+  );
+}
