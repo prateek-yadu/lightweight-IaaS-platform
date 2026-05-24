01922ed98e4b568baa6a31d3bcd7b02c20e73fb6
Prateek Yadu <prateek@prateekyadu.com>
2026-05-24 18:19:36 +0530

    feat(frontend): rendering navbar and footer on needed routes only

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,14 +1,25 @@
-import { ThemeProvider } from '@/components/theme-provider';
-import { Toaster } from '@/components/ui/sonner';
-import { Outlet } from 'react-router';
+import Footer from "@/components/custom/Footer";
+import Navbar from "@/components/custom/Navbar";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+import { Outlet, useLocation } from "react-router";
 
 export default function Layout() {
-    return (
-        <>
-        <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
-            <Toaster position='top-center' duration={1500}/> {/* sonner toast */}
-            <Outlet />
-        </ThemeProvider>
-        </>
-    );
+  const location = useLocation();
+  const currentURL = location.pathname;
+
+  const renderNavbar = () => {
+    return !currentURL.includes("/dashboard") && !currentURL.includes("/login") && !currentURL.includes("/register");
+  };
+
+  return (
+    <>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <Toaster position="top-center" duration={1500} /> {/* sonner toast */}
+        {renderNavbar() && <Navbar />}
+        <Outlet />
+        {renderNavbar() && <Footer />}
+      </ThemeProvider>
+    </>
+  );
 }
