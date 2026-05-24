31b3ee91cf46bff0e3be5b4951d782e61c707839
Prateek Yadu <prateek@prateekyadu.com>
2026-05-24 18:18:38 +0530

    chore(frontend): added empty billing page design

--- frontend/src/pages/dashboard/billings/Billings.tsx
+++ frontend/src/pages/dashboard/billings/Billings.tsx
@@ -1,8 +1,10 @@
 import DashboardHeader from "@/components/dashboard/dashboard-header";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Empty, EmptyContent, EmptyDescription, EmptyHeader, EmptyTitle } from "@/components/ui/empty";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { useEffect, useState } from "react";
+import { Link } from "react-router";
 import { toast } from "sonner";
 
 interface UserPlan {
@@ -66,38 +68,54 @@ export default function Billing() {
     <>
       <DashboardHeader title="Subscriptions" />
 
-      <Table>
-        <TableCaption>List of all Purchased Plans</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Plan Name</TableHead>
-            <TableHead>Purchase date</TableHead>
-            <TableHead>Expiration date</TableHead>
-            <TableHead>Action</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {plans?.map((plan) => (
-            <TableRow key={plan.id}>
-              <TableCell className="flex items-center justify-start h-[52px] gap-2">
-                {plan.name} {plan.in_use === 1 && <Badge variant={"secondary"}>in use</Badge>}
-              </TableCell>
-              <TableCell>{formatDate(plan.purchased_at)}</TableCell>
-              <TableCell>{formatDate(plan.expires_at)}</TableCell>
-              <TableCell>
-                <Button
-                  variant={"default"}
-                  onClick={() => {
-                    renewPlan(plan.id);
-                  }}
-                >
-                  Renew
-                </Button>
-              </TableCell>
+      {plans !== null ? (
+        <Table>
+          <TableCaption>List of all Purchased Plans</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Plan Name</TableHead>
+              <TableHead>Purchase date</TableHead>
+              <TableHead>Expiration date</TableHead>
+              <TableHead>Action</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+          <TableBody>
+            {plans?.map((plan) => (
+              <TableRow key={plan.id}>
+                <TableCell className="flex items-center justify-start h-[52px] gap-2">
+                  {plan.name} {plan.in_use === 1 && <Badge variant={"secondary"}>in use</Badge>}
+                </TableCell>
+                <TableCell>{formatDate(plan.purchased_at)}</TableCell>
+                <TableCell>{formatDate(plan.expires_at)}</TableCell>
+                <TableCell>
+                  <Button
+                    variant={"default"}
+                    onClick={() => {
+                      renewPlan(plan.id);
+                    }}
+                  >
+                    Renew
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      ) : (
+        <Empty className="py-48 border mb-8">
+          <EmptyHeader>
+            <EmptyTitle>No Plans Found.</EmptyTitle>
+            <EmptyDescription>
+              We could not find any Plan linked with your account. Would you like to pruchase one?
+            </EmptyDescription>
+          </EmptyHeader>
+          <EmptyContent className="flex-row justify-center gap-2">
+            <Button>
+              <Link to={"/pricing"}>Purchase Plan</Link>
+            </Button>
+          </EmptyContent>
+        </Empty>
+      )}
     </>
   );
 }
